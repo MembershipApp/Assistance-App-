11bda902fea966f95bbc7e93c0bece5d5f49c439
Christopher Akers <cakers@Christophers-MacBook-Pro-2.local>
2013-05-15 12:49:21 -0400

    Prod and Test notifications file updated

--- test/controllers/Notifications.class.js
+++ test/controllers/Notifications.class.js
@@ -1,6 +1,6 @@
 /* Require module */
 module.exports = {
-    notifications: function(rootPath) { 		       
+    notifications: function(rootPath) {        
         return Notifications(rootPath);
     }
 }
@@ -10,15 +10,16 @@ module.exports = {
  * handles notifications for iOS, Android and BlackBerry 
  */ 
 var Notifications = function(rootPath) {
-	
+    
 	/* variables */	
 	var apn = require('apn');
 	var gcm = require('node-gcm');
+    var bbp  = require('blackberry-push');
 	var geocoder = require('geocoder');
 	this.deviceType;
 	this.token;
 	this.timestamp;
-	
+	this.state;
 	
 	__construct = function() {
 		console.log(rootPath);
@@ -31,27 +32,32 @@ var Notifications = function(rootPath) {
 	/* get address from lat long */
 	this.geo = function(lat , long) {		
 		// handle the geocoding 
-		geocoder.reverseGeocode( lat , long , function ( err, dt ) {								
-				
-				var address = (dt) ? dt.results[0].formatted_address : 'No Lat Long Provide Alert';				
-				
-				console.log(address);
-				
-					
-				if(deviceType === 'i'){
-					sendAPNS(0 , address);	
-					console.log("iOS Device Type");
-				}
-				else if(deviceType === 'A'){
-					sendGCM(address);
-					console.log("Android Device Type");
-				}
-				else if(deviceType === 'BLACKBERRY'){
-					
-				}
-				// Now we need to store it! MongoDB below...
-												
-		});		
+        console.log('The lat: '+lat+'  The long: '+long);
+		if(parseInt(lat) !== 0 && parseInt(long) !== 0) {
+                geocoder.reverseGeocode( lat , long , function ( err, dt ) {								
+        				
+        				var address = (dt) ? dt.results[0].formatted_address : 'No Lat Long Provide Alert';				
+        				
+        				console.log(address);
+        				
+        					
+        				if(deviceType === 'i'){
+        					sendAPNS(0 , address);	
+        					console.log("iOS Device Type");
+        				}
+        				else if(deviceType === 'A'){
+        					sendGCM(address);
+        					console.log("Android Device Type");
+        				}
+        				else if(deviceType === 'BB'){
+        					sendBBPS(address);
+                            console.log("Blackberry Device Type");
+        				}
+                        
+        				// Now we need to store it! MongoDB below...
+        												
+        		});
+		}
 	};
 	
 	
@@ -62,16 +68,23 @@ var Notifications = function(rootPath) {
 	};
 	
 	
-	
 	/* send a Apple push notifications */
 	var sendAPNS = function(badgeNum , address) {
 			var myDevice = new apn.Device(token);
-			var note = new apn.Notification();	
-			
-			note.badge = badgeNum;
+			var note = new apn.Notification();				
+            var now = new Date(); 
+            var date = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+		date = date.toString().split("GMT")[0];	
+            console.log(date);
+            
+            note.badge = badgeNum;
 			note.sound = "default";
-			note.alert = "You checked in near "+address+" $$ "+timestamp;
-			note.device = myDevice;
+			note.alert = (this.state === "foreground") ? 
+            "You checked in near "+address : 
+            "You checked in near "+address+"\n"+date ;
+			
+            note.device = myDevice;
+            note.timestamp = timestamp;
 					 
 			// handle error
 			var callback = function(errorNum, notification){
@@ -101,7 +114,7 @@ var Notifications = function(rootPath) {
 			var registrationIds = [];
 			
 			// Optional
-			message.addData('message', "You checked in near "+address);			
+			message.addData('message', "You checked in near "+address+" $$"+timestamp);			
 			message.collapseKey = 'demo';
 			//message.delayWhileIdle = true;
 			message.timeToLive = 3;
@@ -120,6 +133,23 @@ var Notifications = function(rootPath) {
 				console.log(result);
 			});		
 	};
+    
+    
+    var sendBBPS = function(address) {                        
+            //https://pushapi.eval.blackberry.com/mss/PD_pushRequest
+            
+            var push = bbp('https://pushapi.eval.blackberry.com/mss/PD_pushRequest', 
+                        '3131-l5c0059eyee03ro92666558cc0e53568568',
+                        '8nKLVOck');
+        
+            console.log(token);
+            
+            push(token, "You checked in near "+address+" $$"+timestamp, function(err, result) {
+                console.log('The results : ' + result+ '  :'+err); 
+            });
+           
+           //{ code: '1001', desc: 'The request has been accepted for processing.' }        
+    }
 	
 	
 	__construct();
